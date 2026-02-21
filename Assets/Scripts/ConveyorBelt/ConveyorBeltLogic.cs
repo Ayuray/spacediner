43c9e63339fd6469280520df8a2e771007123b0a
Sifodya <cedric.schlegel@bluewin.ch>
2026-02-21 19:16:25 +0100

    Update conveyor | dynamic item count | begin on check distance

--- Assets/Scripts/ConveyorBelt/ConveyorBeltLogic.cs
+++ Assets/Scripts/ConveyorBelt/ConveyorBeltLogic.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental;
 
 public class ConveyorBeltLogic : MonoBehaviour
@@ -12,6 +13,7 @@ public class ConveyorBeltLogic : MonoBehaviour
         public Transform item;
         [HideInInspector] public float currentLerp;
         [HideInInspector] public int startPoint;
+        //public GameObject _PrefabItem;
     }
 
     public static event Action<Transform, ConveyorBeltItem> OnConveyorEnd;
@@ -19,28 +21,35 @@ public class ConveyorBeltLogic : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private List<ConveyorBeltItem> _items;
+    [SerializeField] private int itemCount;
+    [SerializeField] private GameObject _PrefabItem;
 
     void Start()
     {
         OnConveyorEnd += teleportItem;
+        _items = new List<ConveyorBeltItem>(itemCount);
+        for (int i = 0; i < itemCount; i++)
+        {
+            _items.Add(new ConveyorBeltItem());
+            _items[i].item = Instantiate(_PrefabItem, transform, false).transform;
+        }
     }
     
     private void FixedUpdate()
     {
-        int maxItems = _items.Count;
+        CheckAmountOfItems();
         float lineLength = Vector3.Distance(_lineRenderer.GetPosition(0), _lineRenderer.GetPosition(1));
-        itemSpacing = lineLength / maxItems;
+        itemSpacing = lineLength / itemCount;
+        
         
         for (int i = 0; i < _items.Count; i++)
         {
             ConveyorBeltItem beltItem = _items[i];
             Transform item = beltItem.item;
 
-            if (i > 0)
-            {
-                if(Vector3.Distance(item.position, _items[i - 1].item.position) <= itemSpacing)
-                    continue;
-            }
+            if (CheckItemSpacing(i, item))
+                return;
+            
             
             item.transform.position = Vector3.Lerp(_lineRenderer.GetPosition(beltItem.startPoint), _lineRenderer.GetPosition(beltItem.startPoint + 1), beltItem.currentLerp);
             float distance = Vector3.Distance(_lineRenderer.GetPosition(beltItem.startPoint), _lineRenderer.GetPosition(beltItem.startPoint + 1));
@@ -53,9 +62,7 @@ public class ConveyorBeltLogic : MonoBehaviour
                     beltItem.currentLerp = 0;
                     beltItem.startPoint++;
                 }
-                //OnConveyorEnd?.Invoke(item, beltItem);
-                item.transform.position = _lineRenderer.GetPosition(beltItem.startPoint);
-                beltItem.currentLerp = 0;
+                OnConveyorEnd?.Invoke(item, beltItem);
             }
         }
     }
@@ -63,5 +70,43 @@ public class ConveyorBeltLogic : MonoBehaviour
     private void teleportItem(Transform item, ConveyorBeltItem beltItem)
     {
         item.transform.position = _lineRenderer.GetPosition(beltItem.startPoint);
+        beltItem.currentLerp = 0;
+    }
+
+    public void SetItemCount(int newCount)
+    {
+        itemCount = newCount;
+    }
+
+    private void CheckAmountOfItems()
+    {
+        if (itemCount == _items.Count)
+            return;
+        if (itemCount < _items.Count)
+        {
+            Destroy(_items[_items.Count - 1].item.gameObject);
+            _items.RemoveAt(_items.Count - 1);
+        }
+        else
+        {
+            _items.Add(new ConveyorBeltItem());
+            _items[_items.Count - 1].item = Instantiate(_PrefabItem, transform, false).transform;
+        }
+    }
+
+    private bool CheckItemSpacing(int i, Transform item)
+    {
+        bool reset = false;
+        if (i > 0)
+        {
+            if (Vector3.Distance(item.position, _items[i - 1].item.position) <= itemSpacing)
+                reset = true;
+            else
+            {
+                item.position = _items[i - 1].item.position-new Vector3(0, itemSpacing, 0);
+            }
+        }
+
+        return reset;
     }
 }
