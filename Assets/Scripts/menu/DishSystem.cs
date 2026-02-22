95313a7f14a5b1e0f0f810fe9e13b2bc195950da
Ayuray1 <linni8@me.com>
2026-02-22 10:10:45 +0100

    Progress Queue and Dish system

--- Assets/Scripts/menu/DishSystem.cs
+++ Assets/Scripts/menu/DishSystem.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DishSystem : MonoBehaviour
 {
     [SerializeField] GameObject orderReference;
+
+    private void Start()
+    {
+        orderReference.SetActive(false);
+        StartOrder();
+    }
     public void StartOrder()
     {
-
+        Dish dish = GetComponent<DishGenerator>().GenerateDish();
+        if (dish == null)
+        {
+            Debug.Log("test");
+        }
+        orderReference.SetActive(true);
+        Debug.Log(dish.dishName);
+        orderReference.GetComponent<Image>().sprite = dish.icon;
     }
 }
