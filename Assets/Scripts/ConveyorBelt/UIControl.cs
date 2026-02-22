2d10fa5c6d5a5dc82c3fc54ff8ad1f98776472c9
Ayuray1 <linni8@me.com>
2026-02-22 13:13:48 +0100

    Canvas Formatting

---
+++ Assets/Scripts/ConveyorBelt/UIControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public Camera mainCam;
+
+
+    void Update()
+    {
+        float yMin = mainCam.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
+        float yMax = mainCam.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
+        float x = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(x, yMin, 0));
+        lineRenderer.SetPosition(1, new Vector3(x, yMax, 0));
+    }
+}
